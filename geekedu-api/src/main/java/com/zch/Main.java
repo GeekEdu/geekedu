ab99b5e265c29f0831315a68f390b903b6c2a6a8
Poison02 <2069820192@qq.com>
2023-12-08 15:36:55 +0800

    feat: 搭建项目结构（一）

---
+++ geekedu-api/src/main/java/com/zch/Main.java
@@ -0,0 +1,11 @@
+package com.zch;
+
+/**
+ * @author Poison02
+ * @date 2023/12/8
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
