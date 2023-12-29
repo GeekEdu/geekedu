f93068b08c7a33ef9d94548f2220b5e708ea19e3
Poison02 <2069820192@qq.com>
2023-12-29 14:10:13 +0800

    新增资源模块相关依赖

---
+++ geekedu-resource/geekedu-media/src/main/java/com/zch/MediaApplication.java
@@ -0,0 +1,11 @@
+package com.zch;
+
+/**
+ * @author Poison02
+ * @date 2023/12/29
+ */
+public class MediaApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
