a269c587dc5c7236d3715fd8b824c9b2d902f9dd
Poison02 <2069820192@qq.com>
2024-01-04 22:19:48 +0800

    修改bootstrap.yml

--- geekedu-resource/src/main/java/com/zch/oss/ResourceApplication.java
+++ geekedu-resource/src/main/java/com/zch/oss/ResourceApplication.java
@@ -1,5 +1,6 @@
 package com.zch.oss;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2024/1/4
  */
 @SpringBootApplication
+@MapperScan({"com.zch.oss.mapper"})
 public class ResourceApplication {
 
     public static void main(String[] args) {
