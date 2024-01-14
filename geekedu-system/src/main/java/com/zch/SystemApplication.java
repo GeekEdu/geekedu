57fa673762baca2cfef1f6b6ed3906d63a065aec
Poison02 <2069820192@qq.com>
2024-01-14 17:08:16 +0800

    🎊添加系统模块

---
+++ geekedu-system/src/main/java/com/zch/SystemApplication.java
@@ -0,0 +1,11 @@
+package com.zch;
+
+/**
+ * @author Poison02
+ * @date 2024/1/14
+ */
+public class SystemApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
