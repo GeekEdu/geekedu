0ae289f68ae60016e55e1af1bda908382fc169d1
Poison02 <2069820192@qq.com>
2024-01-17 19:50:41 +0800

    🎊修改网关过滤器，但是不能捕捉到未登录请求，有bug

--- geekedu-gateway/src/main/java/com/zch/gateway/config/SaTokenConfigure.java
+++ geekedu-gateway/src/main/java/com/zch/gateway/config/SaTokenConfigure.java
@@ -1,12 +1,15 @@
 package com.zch.gateway.config;
 
+import cn.dev33.satoken.context.SaHolder;
 import cn.dev33.satoken.reactor.context.SaReactorSyncHolder;
 import cn.dev33.satoken.reactor.filter.SaReactorFilter;
+import cn.dev33.satoken.router.SaHttpMethod;
 import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.dev33.satoken.util.SaResult;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.server.ServerWebExchange;
 
 /**
@@ -15,7 +18,7 @@ import org.springframework.web.server.ServerWebExchange;
  * @author Poison02
  * @date 2024/1/16
  */
-// @Configuration
+@Configuration
 public class SaTokenConfigure {
 
     @Bean
@@ -28,15 +31,39 @@ public class SaTokenConfigure {
                     // 登录校验 -- 拦截所有路由，并排除 登录和验证码 接口
                     SaRouter.notMatch("/user/api/captcha/image")
                             .notMatch("/user/api/login")
-                            .check(r -> StpUtil.checkLogin());
+                            .check(r -> {
+                                StpUtil.checkLogin();
+                                ServerHttpRequest request = SaReactorSyncHolder.getContext().getRequest();
+                                String token = request.getHeaders().getFirst("Bearer");
+                                System.out.println("======token======为" + token);
+                            });
                 })
                 .setError(e -> {
                     // 设置错误返回格式为JSON
                     ServerWebExchange exchange = SaReactorSyncHolder.getContext();
                     exchange.getResponse().getHeaders().set("Content-Type", "application/json: charset=utf-8");
                     return SaResult.error(e.getMessage());
-                });
+                })
+                // 前置函数：在每次认证函数之前执行
+                .setBeforeAuth(obj -> {
+                    SaHolder.getResponse()
 
+                            // ---------- 设置跨域响应头 ----------
+                            // 允许指定域访问跨域资源
+                            .setHeader("Access-Control-Allow-Origin", "*")
+                            // 允许所有请求方式
+                            .setHeader("Access-Control-Allow-Methods", "*")
+                            // 允许的header参数
+                            .setHeader("Access-Control-Allow-Headers", "*")
+                            // 有效时间
+                            .setHeader("Access-Control-Max-Age", "3600")
+                    ;
+
+                    // 如果是预检请求，则立即返回到前端
+                    SaRouter.match(SaHttpMethod.OPTIONS)
+                            .free(r -> System.out.println("--------OPTIONS预检请求，不做处理"))
+                            .back();
+                });
     }
 
 }
