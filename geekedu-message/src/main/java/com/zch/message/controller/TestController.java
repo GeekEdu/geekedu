58ab9eae5fc77ffa2913a1c98909b0bc17fef254
Poison02 <2069820192@qq.com>
2024-03-08 19:37:37 +0800

    🎊测试rocketmq成功

--- geekedu-message/src/main/java/com/zch/message/controller/TestController.java
+++ geekedu-message/src/main/java/com/zch/message/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.zch.message.controller;
 
-import org.apache.rocketmq.spring.core.RocketMQTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.zch.message.domain.User;
+import com.zch.message.listener.MQProducerService;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author Poison02
@@ -13,14 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class TestController {
 
-    @Autowired
-    private RocketMQTemplate rocketMQTemplate;
+    @Resource
+    private MQProducerService mqProducerService;
 
-    @RequestMapping("/test")
-    public String order() {
-        String test = "123测试";
-        rocketMQTemplate.convertAndSend("ORDER_TOPIC", test);
-        return test;
+    @GetMapping("/send")
+    public void send() {
+        User user = new User();
+        user.setName("nameIsOk");
+        user.setAge(100);
+        mqProducerService.send(user);
+    }
+
+    @GetMapping("/sendTag")
+    public SendResult sendTag() {
+        SendResult sendResult = mqProducerService.sendTagMsg("带有tag的字符消息");
+        return sendResult;
     }
 
 }
---
+++ geekedu-message/src/main/java/com/zch/message/domain/User.java
@@ -0,0 +1,30 @@
+package com.zch.message.domain;
+
+/**
+ * @author Poison02
+ * @date 2024/3/8
+ */
+public class User {
+
+    private String name;
+
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
+}
